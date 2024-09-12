0276aa37f82384518270e68bd86233292f893b4b
haebel7 <ahaeberlin7@gmx.ch>
2024-09-12 18:35:41 +0200

    NinjaCollection hooked up to ninjacounter

--- Assets/Scripts/NinjaCollection.cs
+++ Assets/Scripts/NinjaCollection.cs
@@ -8,19 +8,22 @@ public class NinjaCollection : MonoBehaviour
     [SerializeField] private Sprite cloneSpriteSingle;
     [SerializeField] private Sprite cloneSpriteDouble;
     [SerializeField] private Sprite cloneSpriteQuad;
-    [SerializeField] private float cloningForce = 5;
+    [SerializeField] private NinjaCounter ninjaCounter;
 
     private List<Transform> clones = new List<Transform>();
 
-    // TODO: Replace with counter from scriptable object
-    private int cloneCount = 0;
+    void OnEnable()
+    {
+        ninjaCounter.ninjaCount = 0;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject newClone = Instantiate(ninjaClone, transform.position, transform.rotation);
+        newClone.transform.parent = transform;
         clones.Add(newClone.transform);
-        cloneCount++;
+        ninjaCounter.ninjaCount++;
     }
 
     // Update is called once per frame
@@ -29,24 +32,32 @@ public class NinjaCollection : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             DestroyClone(clones[Random.Range(0, clones.Count)]);
-            print(cloneCount);
+            print(ninjaCounter.ninjaCount);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            if (ninjaCounter.ninjaCount > 900)
+            {
+                ninjaCounter.ninjaCount = ninjaCounter.ninjaCount * 2;
+                // TODO: Add code that doubles the value of each clone (accessed in their future ninjaClone script)
+                print(ninjaCounter.ninjaCount);
+                return;
+            }
+
             List<Transform> newClones = new List<Transform>();
             foreach (Transform clone in clones)
             {
-                if (cloneCount < 20
-                    || (cloneCount < 120 && Random.value < 0.3)
+                if (ninjaCounter.ninjaCount < 20
+                    || (ninjaCounter.ninjaCount < 120 && Random.value < 0.3)
                     || Random.value < 0.1
                     || clone.GetComponent<SpriteRenderer>().sprite == cloneSpriteQuad)
                 {
                     GameObject newClone = Instantiate(ninjaClone, clone.position, transform.rotation);
+                    newClone.transform.parent = transform;
                     newClones.Add(newClone.transform);
-                    newClone.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.3f, 0.7f) * cloningForce, ForceMode2D.Impulse);
                     newClone.GetComponent<Rigidbody2D>().velocity = clone.GetComponent<Rigidbody2D>().velocity;
-                    newClone.transform.position += new Vector3(0.5f,0,0);
+                    newClone.transform.position += new Vector3(0.5f, 0, 0);
                     newClone.GetComponent<NinjaMovement>().CheckIfGrounded();
                     newClone.GetComponent<NinjaMovement>().Jump();
 
@@ -54,15 +65,15 @@ public class NinjaCollection : MonoBehaviour
                     spriteRenderer.sprite = clone.GetComponent<SpriteRenderer>().sprite;
                     if (spriteRenderer.sprite == cloneSpriteSingle)
                     {
-                        cloneCount += 1;
+                        ninjaCounter.ninjaCount += 1;
                     }
                     else if (spriteRenderer.sprite == cloneSpriteDouble)
                     {
-                        cloneCount += 2;
+                        ninjaCounter.ninjaCount += 2;
                     }
                     else
                     {
-                        cloneCount += 4;
+                        ninjaCounter.ninjaCount += 4;
                     }
                 }
                 else
@@ -71,18 +82,18 @@ public class NinjaCollection : MonoBehaviour
                     if (spriteRenderer.sprite == cloneSpriteSingle)
                     {
                         spriteRenderer.sprite = cloneSpriteDouble;
-                        cloneCount += 1;
+                        ninjaCounter.ninjaCount += 1;
                     }
                     else
                     {
                         spriteRenderer.sprite = cloneSpriteQuad;
-                        cloneCount += 2;
+                        ninjaCounter.ninjaCount += 2;
                     }
                 }
             }
             clones.AddRange(newClones);
 
-            print(cloneCount);
+            print(ninjaCounter.ninjaCount);
         }
     }
 
@@ -91,15 +102,15 @@ public class NinjaCollection : MonoBehaviour
         var spriteRenderer = clone.GetComponent<SpriteRenderer>();
         if (spriteRenderer.sprite == cloneSpriteSingle)
         {
-            cloneCount -= 1;
+            ninjaCounter.ninjaCount -= 1;
         }
         else if (spriteRenderer.sprite == cloneSpriteDouble)
         {
-            cloneCount -= 2;
+            ninjaCounter.ninjaCount -= 2;
         }
         else
         {
-            cloneCount -= 4;
+            ninjaCounter.ninjaCount -= 4;
         }
 
         clones.Remove(clone);
